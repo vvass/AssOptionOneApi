88242045340ca1da6abb2abb141737b75fdcdaf9
Vince Vass <vvass@Vinces-MacBook-Pro.local>
2018-02-20 19:48:29 -0500

    transnpose the response to alert case class

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -13,36 +13,41 @@ import play.api.libs.functional.syntax._
 import play.api.libs.json._
 
 
+case class Alerts(name: String, value: Int, date: Long)
 
 class HomeController @Inject() (ws: WSClient) extends InjectedController {
   
   def index = Action.async {
   
-    
-    
     ws.url("https://sample-api.pascalmetrics.com/api/events")
       .addHttpHeaders("Accept" -> "application/json")
       .addQueryStringParameters("api-key" -> "nu11p0int3r!")
       .get().map { response =>
-  
-        case class Alert(names: String, value: Int, date: String)
-    
-        implicit val alertReads: Reads[Alert] = (
-          (__ \ "name").read[String] and
+      
+        implicit val alertReads: Reads[Alerts] = (
+            (__ \ "name").read[String] and
             (__ \ "value").read[Int] and
-            (__ \ "date").read[String]
-          ) ( Alert.apply _ )
+            (__ \ "date").read[Long]
+          ) ( Alerts.apply _ )
       
+        val json: JsValue = Json.parse(response.body.toString)
       
-        {response.json \ ""}.validate[Alert] match {
-          case JsSuccess(a, _) => println(a)
-          case JsError(errors) =>
-            println(errors)
+        val validatedAlerts = json.validate[List[Alerts]]
+        
+        val listAlerts = validatedAlerts match {
+          case JsSuccess(list, _) => list
+          case e: JsError => {
+            println("Error " + e)
+            List()
+          }
         }
       
-        val jsonContent = response.json
+        listAlerts.map( alert =>
+          println(alert.name)
+        )
+        
       
-        Ok(views.html.index(jsonContent.toString))
+        Ok(views.html.index(listAlerts.toString))
         
         
       }
